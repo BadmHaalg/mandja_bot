7210791ebb875fb6a3cc9996d33afc5af143040b
BadmHaalg <badmhaalg@gmail.com>
2022-06-30 21:28:35 +0300

    add advanced search

--- bot/tbot.py
+++ bot/tbot.py
@@ -2,60 +2,99 @@ import telebot
 from telebot import types
 import psycopg2
 
+from utils.fuzzy_wuzzy import standart_fuzzy_wuzzy, custom_fuzzy_wuzzy
 
-bot = telebot.TeleBot('')
+test_bot = '2080377181:AAHZ234eNUgd28wcYQXacMGosjYiqrjbwMI'
 
-lang = ''
+bot = telebot.TeleBot(test_bot)
+
+
+# lang = ''
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     user = message.from_user.username
-    bot.send_message(message.chat.id, f'Менд, {user}! Меня зовут Манджа, я буду твоим помощником в переводе калмыцких слов. Я умею переводить в двух режимах: с русского на калмыцкий и с калмыцкого на русский. Для выбора или изменения режима напиши мне /lang')
+    bot.send_message(message.chat.id,
+                     f'Менд, {user}! Меня зовут Манджа, я буду твоим помощником-переводчиком! Я могу переводить и '
+                     f'русские, и калмыцкие слова. Просто начни мне писать! ')
 
 
-@bot.message_handler(commands=['lang'])
-def lang_buttons(message):
-    keyboard = types.InlineKeyboardMarkup()
-    rus_kalm = types.InlineKeyboardButton(text='Русско-калмыцкий', callback_data='rus_kalm')
-    kalm_rus = types.InlineKeyboardButton(text='Калмыцко-русский', callback_data='kalm_rus')
-    keyboard.add(rus_kalm, kalm_rus)
-    bot.send_message(message.chat.id, 'Выбери режим', reply_markup=keyboard)
+# @bot.message_handler(commands=['lang'])
+# def lang_buttons(message):
+#     keyboard = types.InlineKeyboardMarkup()
+#     rus_kalm = types.InlineKeyboardButton(text='Русско-калмыцкий', callback_data='rus_kalm')
+#     kalm_rus = types.InlineKeyboardButton(text='Калмыцко-русский', callback_data='kalm_rus')
+#     keyboard.add(rus_kalm, kalm_rus)
+#     bot.send_message(message.chat.id, 'Выбери режим', reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda call:True)
-def callback_worker(call):
-    global lang
-    if call.data == 'rus_kalm':
-        bot.send_message(call.message.chat.id, 'Вы выбрали режим перевода с русского на калмыцкий')
-        lang = 'rus_kalm'
-    elif call.data == 'kalm_rus':
-        bot.send_message(call.message.chat.id, 'Вы выбрали режим перевода с калмыцкого на русский')
-        lang = 'kalm_rus'
+# @bot.callback_query_handler(func=lambda call: True) def callback_worker(call): global lang if call.data ==
+# 'rus_kalm': bot.send_message(call.message.chat.id, '''Вы выбрали режим перевода с русского на калмыцкий. Количество
+# словарный статей в данном режиме VAR ''') lang = 'rus_kalm' elif call.data == 'kalm_rus': bot.send_message(
+# call.message.chat.id, '''Вы выбрали режим перевода с русского на калмыцкий. Количество словарный статей в данном
+# режиме VAR ''') lang = 'kalm_rus'
 
+similarity_percentage = 0.1
 
+# сделать режим есть калмыцкие буквы или нет?
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
     con = psycopg2.connect(
-        database='tbot',
-        user='postgres',
-        password="postgres",
+        database='testdb',
+        user='andrey',
+        password="Yashkul08#",
         host="127.0.0.1",
         port="5432"
     )
     cur = con.cursor()
-    try:
-        cur.execute(f"SELECT * FROM {lang} WHERE word='{message.text.lower()}';")
+    try:  # в этом блоке - выбираем  с помощью триграмм.
+        # 1. если есть полное совпадение (индекс 1), то высылаем его (а если несколько)
+        # 2. Если нет полного "сори, но возможно вы имели ввиду .... - список ближайших совпадений"
+        # cur.execute(f"SELECT * FROM all_words WHERE word='{message.text.lower()}' AND type='{lang}';")
+        text = message.text.lower()
+        cur.execute(
+            f"WITH ts AS (SELECT word, article, (similarity(all_words.word, '{text}')) AS similarity, dictionary "
+            f"FROM all_words ORDER BY similarity DESC) SELECT * FROM ts WHERE similarity >= {similarity_percentage} "
+            f"AND LENGTH(word) BETWEEN {len(text)} AND {len(text)+1};")
         row = cur.fetchall()
-        bot.send_message(message.from_user.id, f"{row[0][0]} - {row[0][1]}")
-        con.close()
-    except psycopg2.errors.SyntaxError:
-        bot.send_message(message.from_user.id, 'Вы не выбрали режим. Для выбора режима напишите /lang')
-    except psycopg2.errors.UndefinedTable:
-        bot.send_message(message.from_user.id, 'Режим калмыцко-русского перевода еще не реализован. Для смены напишите /lang')
-    except IndexError:
-        bot.send_message(message.from_user.id, 'Такого слова пока что нет в моих словарях')
+        if row:
+            exact_math = []
+            similar = []
+            for r in row:
+                word = r[0].replace(' ', '')
+                translate = r[1]
+                similarity = r[2]
+                dictionary = r[3]
+                if similarity == 1:
+                    exact_math.append(f"{word} - {translate}. \n ---------- \n Словарь: {dictionary}")
+                else:
+                    similar.append([word, standart_fuzzy_wuzzy(text, word), similarity])
+            if exact_math:
+                for artilce in exact_math:
+                    bot.send_message(
+                        message.from_user.id,
+                        artilce
+                    )
+            else:
+                to_send = custom_fuzzy_wuzzy(similar, text)
+                if to_send:
+                    bot.send_message(message.from_user.id,
+                                     f'К сожалению, я не нашел слова "{text}". Вот похожие на ваш запрос слова:')
+                    bot.send_message(message.from_user.id, to_send)
+                    bot.send_message(message.from_user.id,
+                                     'Если среди них есть нужно вам, пришлите мне его и я его переведу.')
+                else:
+                    raise IndexError
+        else:
+            raise IndexError
 
+        con.close()
+
+    # except psycopg2.errors.SyntaxError:
+    #     bot.send_message(message.from_user.id, 'Вы не выбрали режим. Для выбора режима напишите /lang')
+    except IndexError:
+        bot.send_message(message.from_user.id, 'К сожалению, ничего похожего я не нашел.')
 
 
 bot.polling()
---
+++ utils/fuzzy_wuzzy.py
@@ -0,0 +1,43 @@
+from thefuzz import fuzz
+
+kalm_literals = ['ү', 'ә', 'ң', 'ө', 'һ', 'җ']
+pairs = {
+    'ү': 'у',
+    'Ү': 'ү',
+    'Ә': 'ә',
+    'Ң': 'ң',
+    'Ө': 'ө',
+    'Һ': 'һ',
+    'Җ': 'җ',
+    'ә': 'я',
+    'ң': 'н',
+    'ө': 'о',
+    'һ': 'г',
+    'җ': 'ж',
+}
+
+
+# similarity уже не очень выборку дает иногда
+def standart_fuzzy_wuzzy(query, from_db):
+    return fuzz.WRatio(query, from_db)
+
+
+def custom_fuzzy_wuzzy(similar_list, query_text):
+    output_list = []
+    for el in similar_list:
+        if len(el[0]) == len(query_text):
+            for i, lit in enumerate(el[0]):
+                if lit in kalm_literals and query_text[i] == pairs[lit]:
+                    el[1] += 15
+                    if len(el[0]) == 3:
+                        el[1] += 15
+        if el[1] > 66:
+            output_list.append(el)
+    out = ''
+    a = sorted(output_list, key=lambda x: (x[1], x[2]), reverse=True)
+    print(a)
+    print(similar_list)
+    for word in a:
+        out += word[0] + ', '
+
+    return out[:-2]
